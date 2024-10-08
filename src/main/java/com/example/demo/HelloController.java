1f8292fca6476ff975e0428f1896ff267d01c5f1
apple6346654 <apple6346654@naver.com>
2024-10-08 16:54:46 +0900

    5

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Nihaohhhh, World!";
+        return "Nihaohhhhooooooooo, World!";
     }
 }
